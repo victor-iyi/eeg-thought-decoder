6e83adb070a6b27883c7d63a3ca97b28cc21d993
Victor I. Afolabi <javafolabi@gmail.com>
2024-09-19 21:54:17 +0100

    Add the Agentic model for dynamic adaptation

--- src/thought_decoder/models/__init__.py
+++ src/thought_decoder/models/__init__.py
@@ -1,6 +1,7 @@
+from thought_decoder.models.agentic.policy import AgenticModel
 from thought_decoder.models.transformer.encoder import TransformerEncoder
 from thought_decoder.models.gnn.graph_nn import GNN
 from thought_decoder.models.moe.mixture_of_experts import MixtureOfExperts
 
 
-__all__ = ['GNN', 'MixtureOfExperts', 'TransformerEncoder']
+__all__ = ['AgenticModel', 'GNN', 'MixtureOfExperts', 'TransformerEncoder']
